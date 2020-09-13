a89cd5f769757183cd5a3b73d4895b4a04298230
Damir <dtemi2@uis.edu>
2020-09-13 08:50:59 -0500

    Authorization complete

--- personal_site/authorization/urls.py
+++ personal_site/authorization/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import UserRegisterView
 
-app_name = 'authorizationn'
+app_name = 'authorization'
 
 urlpatterns = [
     path('register/', UserRegisterView.as_view(), name='register'),
