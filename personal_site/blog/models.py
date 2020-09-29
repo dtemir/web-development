a2a3d961e196cb4f8a975aa2508fcd5e74c3f766
Damir <dtemi2@uis.edu>
2020-09-29 12:34:32 -0500

    Added ckeditor as a part of the Post Model to have a better editor

--- personal_site/blog/models.py
+++ personal_site/blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
+from ckeditor.fields import RichTextField
 
 # Create your models here.
 STATUS = (
@@ -15,7 +16,8 @@ class Post(models.Model):
     slug = models.SlugField(max_length=100, unique=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
     updated_on = models.DateTimeField(auto_now=True)
-    content = models.TextField()
+    content = RichTextField(blank=True, null=True)
+    # content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
     category = models.CharField(max_length=255, default='Coding')
