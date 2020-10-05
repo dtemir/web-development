35b879c95040f4dfb02ecafcc492df01f56f009a
Damir <dtemi2@uis.edu>
2020-10-05 16:00:17 -0500

    Added form fields to authorization app, tweaked gitignore

--- personal_site/authorization/views.py
+++ personal_site/authorization/views.py
@@ -1,12 +1,11 @@
 from django.shortcuts import render
 from django.views import generic
 from django.contrib.auth.forms import UserCreationForm
+from . import forms
 from django.urls import reverse_lazy
 
 
 class UserRegisterView(generic.CreateView):
-    form_class = UserCreationForm
+    form_class = forms.SignUpForm
     template_name = 'registration/register.html'
     success_url = reverse_lazy('login')
-
-# Create your views here.
