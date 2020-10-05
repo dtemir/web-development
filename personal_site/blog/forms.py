a44084b0465bb0879ffae1ea8598318deefc4c6b
Damir <dtemi2@uis.edu>
2020-10-04 20:55:15 -0500

    Changed edit_post form

--- personal_site/blog/forms.py
+++ personal_site/blog/forms.py
@@ -26,4 +26,4 @@ class PostForm(forms.ModelForm):
 class EditForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ('title', 'header_image', 'slug', 'snippet', 'author', 'category', 'content', 'status')
+        fields = ('title', 'header_image', 'slug', 'snippet', 'category', 'content', 'status')
