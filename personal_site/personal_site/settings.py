b401f60adee96d729fe8cdcd4a526bd3e044c9bb
Damir <dtemi2@uis.edu>
2020-11-01 13:11:09 -0600

    Now all static and media files are served from S3

--- personal_site/personal_site/settings.py
+++ personal_site/personal_site/settings.py
@@ -135,9 +135,6 @@ LOGOUT_REDIRECT_URL = 'blog:home'
 
 STATIC_URL = '/static/'
 
-MEDIA_URL = '/images/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
-
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 AWS_LOCATION = 'static'
@@ -148,7 +145,7 @@ AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 AWS_S3_OBJECT_PARAMETERS = {
      'CacheControl': 'max-age=86400',
 }
-DEFAULT_FILE_STORAGE = 'app.storage_backends.MediaStorage'
+DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
@@ -161,4 +158,5 @@ STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 )
 AWS_DEFAULT_ACL = None
+AWS_S3_FILE_OVERWRITE = False
 
