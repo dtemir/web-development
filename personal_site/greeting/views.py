d4ee6412c1ecc64c13a610637f9087d16d2baf62
Damir <dtemi2@uis.edu>
2020-11-05 18:12:10 -0600

    Added coding section on index page

--- personal_site/greeting/views.py
+++ personal_site/greeting/views.py
@@ -6,15 +6,16 @@ from blog.models import Post
 def IndexView(request):
     template_name = 'greeting/index.html'
 
-    all_posts = Post.objects.filter(status=1).order_by('-created_on')
     project_posts = Post.objects.filter(status=1, category='Projects').order_by('-created_on')
     general_posts = Post.objects.filter(status=1, category='General').order_by('-created_on')
+    coding_posts = Post.objects.filter(status=1, category='Coding').order_by('-created_on')
     college_posts = Post.objects.filter(status=1, category='College').order_by('-created_on')
     travelling_posts = Post.objects.filter(status=1, category='Travelling').order_by('-created_on')
 
-    return render(request, template_name, {'post_list': all_posts,
+    return render(request, template_name, {
                                            'projects_list': project_posts,
                                            'general_list': general_posts,
+                                           'coding_list': coding_posts,
                                            'college_list': college_posts,
                                            'travelling_list': travelling_posts,
                                            })
