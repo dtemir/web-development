c50de7a2e7d467cff6c1261abb38134abd8595cc
Damir <dtemi2@uis.edu>
2020-11-10 18:38:14 -0600

    Transfered to Debug=False state

--- personal_site/personal_site/settings.py
+++ personal_site/personal_site/settings.py
@@ -12,9 +12,14 @@ TEMPLATES_DIRS = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+SECURE_SSL_REDIRECT = True
 
 ALLOWED_HOSTS = ['*']
+USE_X_FORWARDED_PORT = True
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = SECRET_KEY_config
