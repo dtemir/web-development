c70678f3c3acada679553a4af6b7cfc5ac91b15a
Damir <dtemi2@uis.edu>
2021-01-05 21:35:08 -0600

    Created a description model for the index page

--- personal_site/greeting/models.py
+++ personal_site/greeting/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from ckeditor.fields import RichTextField
+
 
 # Create your models here.
+
+class Description(models.Model):
+    text = RichTextField(blank=True, null=True)
