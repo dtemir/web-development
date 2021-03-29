87ebfc824c7d73344e34dcdfd25095fd53156d9c
Damir <dtemi2@uis.edu>
2021-03-28 19:13:37 -0500

    Created Experience model for the greeting application

--- personal_site/greeting/models.py
+++ personal_site/greeting/models.py
@@ -1,9 +1,13 @@
 from django.db import models
 from ckeditor.fields import RichTextField
 
-
 # Create your models here.
 
 class Description(models.Model):
     text = RichTextField(blank=True, null=True)
     created_on = models.DateTimeField(auto_now_add=True)
+
+class Experience(models.Model):
+    title = models.CharField(max_length=100, unique=True)
+    description = RichTextField(blank=True, null=True, config_name='default')
+    period = models.CharField(max_length=100, unique=True)
